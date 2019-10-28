b0a7e71be693569af15784e1c01d9edc8c9571fd
Datner <yuval.datner.alt@gmail.com>
2019-10-28 15:54:56 +0200

    Updates

---
+++ precache-manifest.b83330a9762947fd948a61d422777700.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62c4afd0402bf3f8b811e0311eea09a",
+    "url": "/react-color-selection-streamelements/index.html"
+  },
+  {
+    "revision": "88d796e07ae6f4cfa8af",
+    "url": "/react-color-selection-streamelements/static/css/main.338e0d68.chunk.css"
+  },
+  {
+    "revision": "3a2b69ad6f69ba288c49",
+    "url": "/react-color-selection-streamelements/static/js/2.bb4125ff.chunk.js"
+  },
+  {
+    "revision": "88d796e07ae6f4cfa8af",
+    "url": "/react-color-selection-streamelements/static/js/main.874f2275.chunk.js"
+  },
+  {
+    "revision": "5120a8f5b1dda4eb1385",
+    "url": "/react-color-selection-streamelements/static/js/runtime-main.912b4e4c.js"
+  }
+]);
