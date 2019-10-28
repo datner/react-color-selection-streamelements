57773bfe0da3c04c1a631e422130e3d6506eed32
Datner <yuval.datner.alt@gmail.com>
2019-10-28 13:12:57 +0200

    attach Swatch to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import useStyles from './App.styles'
 import Picker from 'components/Picker';
+import Swatch from 'components/Swatch';
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.App}>
       <Picker/>
+      <Swatch />
     </div>
   );
 }
