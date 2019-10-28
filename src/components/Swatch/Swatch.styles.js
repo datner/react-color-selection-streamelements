44b5ac17749205620000890bcbab6e6e9c794928
Datner <yuval.datner.alt@gmail.com>
2019-10-28 13:12:18 +0200

    create Swatch component
    add a placeholder for swatch

---
+++ src/components/Swatch/Swatch.styles.js
@@ -0,0 +1,11 @@
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+    Swatch: {
+        width: 100,
+        height: 50,
+        border: "1px solid black"
+    }
+})
+
+export default useStyles;
